7959a661973600fda99c85147f6b4d54eaf1cd3a
Ashraful <ashrafulmomin7076@gmail.com>
2025-06-19 12:40:23 +0530

    Add Google Search Console verification file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
-})
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'googlee143be000ba5d6f8.html', // ✅ Your verification file
+          dest: '' 
+        }
+      ]
+    })
+  ]
+});
